bd955e09b19113ad092e3eab1655990eb4bcf0c1
Hyy <1819588250@qq.com>
2023-03-16 09:03:05 +0800

    fix map strategy_dict key to double in Robot.h

--- C++/Robot.h
+++ C++/Robot.h
@@ -21,7 +21,7 @@ public:
     double y_;
     int todo_type;
     vector<tuple<double, double, int>> task_list;
-    map<int, double> strategy_dict;
+    map<double, double> strategy_dict;
     int last_assigned_time;
 
 
